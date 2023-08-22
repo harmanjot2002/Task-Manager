dfd46a22d293219c7fb29a830e13322f937344ad
harmanjot2002 <harmanjot0555.be21@chitkara.edu.in>
2023-08-22 22:31:43 +0530

    Added status completed functionality

--- Filtering/filter.js
+++ Filtering/filter.js
@@ -57,7 +57,7 @@ const filterItems = (a)=>{
     
 const displayItem = (items) => {
     document.getElementById('main').innerHTML = items.map((item)=>{
-        var {category,title,desc,ind,date} = item;
+        var {category,title,desc,ind,date,ischeck} = item;
         const data = JSON.parse(localStorage.getItem("mynotes")) || [];
         let num = "";
         if(ind !== undefined) num = ind;
@@ -69,7 +69,7 @@ const displayItem = (items) => {
             <div class="card">
                 <div class="icons">
                     <input onchange="handleCheckBox(event,${num})" type="checkbox" class=" ${"complete"+num} complete" id="ci" ${ischeck && "checked"} >
-                    <input type="text" autocomplete="off" class="${"category"+num} category" placeholder="Add catgory" id="category" value=${category ? category : str}>
+                    <input style="${ischeck ? "text-decoration:line-through 3px var(--main-bg)" : "text-decoration:none" }" type="text" autocomplete="off" class="${"category"+num} category" placeholder="Add category" id="category" value=${category ? category : str}>
                     <i onclick="saveNotes(${num},${num},${num},${num})" class="save fas fa-save"></i>
                     <i onclick="deleteNotes(${num})" class="trash fas fa-trash"></i> 
                 </div>
--- script.js
+++ script.js
@@ -43,7 +43,7 @@ const sortDate = () => {
         });
     }
     document.getElementById('main').innerHTML = newData.map((item)=>{
-        var {category,title,desc,ind,date} = item;
+        var {category,title,desc,ind,date,ischeck} = item;
         const data = JSON.parse(localStorage.getItem("mynotes")) || [];
         let num = "";
         if(ind !== undefined) num = ind;
@@ -55,7 +55,7 @@ const sortDate = () => {
             <div class="card">
                 <div class="icons">
                     <input onchange="handleCheckBox(event,${num})" type="checkbox" class=" ${"complete"+num} complete" id="ci" ${ischeck && "checked"} >
-                    <input type="text" autocomplete="off" class="${"category"+num} category" placeholder="Add catgory" id="category" value=${category ? category : str}>
+                    <input style="${ischeck ? "text-decoration:line-through 3px var(--main-bg)" : "text-decoration:none" }" type="text" autocomplete="off" class="${"category"+num} category" placeholder="Add category" id="category" value=${category ? category : str}>
                     <i onclick="saveNotes(${num},${num},${num},${num})" class="save fas fa-save"></i>
                     <i onclick="deleteNotes(${num})" class="trash fas fa-trash"></i> 
                 </div>
@@ -156,7 +156,7 @@ const addNote = (category,title,desc,ind,date,ischeck) => {
     note.innerHTML = ` 
     <div class="icons">
         <input onchange="handleCheckBox(event,${num})" type="checkbox" class=" ${"complete"+num} complete" id="ci" ${ischeck && "checked"} >
-       <input type="text" autocomplete="off" class="${"category"+num} category" placeholder="Add category" id="category" value=${category ? category : str}>
+        <input style="${ischeck ? "text-decoration:line-through 3px var(--main-bg)" : "text-decoration:none" }" type="text" autocomplete="off" class="${"category"+num} category" placeholder="Add category" id="category" value=${category ? category : str}>
         <i onclick="saveNotes(${num},${num},${num},${num})" class="save fas fa-save"></i>
         <i onclick="deleteNotes(${num})" class="trash fas fa-trash"></i> 
     </div>
