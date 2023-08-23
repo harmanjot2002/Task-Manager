7571fe79966db4b0b7692e4c4929b4a2db0b6b95
harmanjot2002 <harmanjot0555.be21@chitkara.edu.in>
2023-08-24 00:35:25 +0530

    Added calender

--- script.js
+++ script.js
@@ -190,3 +190,11 @@ const deleteNotes = (num)=>{
         }
     }
 )()
+
+
+function toggle() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
